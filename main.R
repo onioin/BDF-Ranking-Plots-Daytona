f083eead52c9366467ce58e06db25796343ed8a8
onioin <canyongudaitis@gmail.com>
2025-01-29 09:55:41 -0800

    Read in, clean, and join data

--- main.R
+++ main.R
@@ -3,9 +3,29 @@ require(ggplot2)
 require(readr)
 require(magrittr)
 
-RANKINGS_PATH <- "./TORA/daytona/rankings.csv"
-ENTRIES_PATH  <- "./TORA/daytona/"
+RANKINGS_PATH <- "./rankings.csv"
+ENTRIES_PATH  <- "./entries.csv"
 
+
+# Read in BDF's Rankings
+# Clean up unused data
+# Change lobby and class to factors (easier grouping)
+# Remove drivers whose lobby is 'N/A'
 TORARankings <- read_csv(RANKINGS_PATH, col_select=(3:7)) %>%
-  mutate(across(c(1,2)), as.factor) %>% 
+  mutate(across(c(1,2), as.factor)) %>% 
   filter(as.integer(LBY) != 5)
+
+# Read in the entry list
+# Clean up unused and duplicate data
+# Change car to factor (easier grouping)
+# Rename columns to match BDF's standard
+TORAEntries <- read_csv(ENTRIES_PATH, skip=3, col_select=c(1,3)) %>%
+  mutate(across(2, as.factor)) %>%
+  mutate(TEAM=`Team Name...1`, CAR=`Car Choice...3`) %>%
+  select(all_of(c("TEAM", "CAR")))
+
+# Perform a natural join on the two datasets
+# Essentially adding which car the team drove to the table
+TORARankings %<>% merge(TORAEntries, sort=FALSE)
+
+
